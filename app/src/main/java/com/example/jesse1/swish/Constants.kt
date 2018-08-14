c937277ef7b008b1883c07cb19c67a464d5bc4f2
Jesse Collier <jessecollier90@hotmail.com>
2018-08-13 21:30:17 -0400

    Added button conditionals and data pass-through

---
+++ app/src/main/java/com/example/jesse1/swish/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.jesse1.swish
+
+const val EXTRA_LEAGUE = "league"
--- app/src/main/java/com/example/jesse1/swish/SkillActivity.kt
+++ app/src/main/java/com/example/jesse1/swish/SkillActivity.kt
@@ -1,12 +1,14 @@
 package com.example.jesse1.swish
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 
-class SkillActivity : AppCompatActivity() {
+class SkillActivity : BaseActivity() {
+
+    var league = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+        league = intent.getStringExtra(EXTRA_LEAGUE)
     }
 }
